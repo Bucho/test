06f665f712621fb8eb19316b798f8ecee5fbfcf0
Bucho <egor.gorokhovtsev@gmail.com>
2010-03-03 22:07:04 +0300

    Many things changed

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -12,11 +12,45 @@ class SlideshowsController < ApplicationController
   
   def edit
     @slideshow = Slideshow.find(params[:id])
+    session[:slideshow] = @slideshow
+    @photos = unused_photos(@slideshow)
+  end
+  
+  def unused_photos(slideshow)
+    all_photos = Photo.find(:all)
+    candidates = []
+    for photo in all_photos
+       in_slideshow = false
+       for slide in slideshow.slides
+         if slide.photo.thumbnail === photo.thumbnail
+            in_slideshow = true
+            break
+          end
+       end
+       candidates << photo if not in_slideshow
+    end
+    return candidates
+  end
+
+  def update_slide_order
+    params[:sortable_thumbs].each_with_index do |id, position|
+      Slide.update(id, :position => position)
+    end
+  end
+  
+  def remove_slide
+    slideshow_id = session[:slideshow].id
+    slide_id = params[:id].split("_")[1]
+    Slide.delete(slide_id)
+    @slideshow = Slideshow.find(slideshow_id)
+    session[:slideshow] = @slideshow
+    @photos = unused_photos(@slideshow)
+    render_partial 'photo_picker'
   end
   
   def update_slide_order
     params[:sortable_thumbs].each_with_index do |id, position|
-      Slide.update(id, :position => position)
+       Slide.update(id, :position => position)
     end
   end
   
