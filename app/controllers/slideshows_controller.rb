f0ffc5ca48c881ed45fea90c3f12d7f89fa5b6a1
bucho <egor.gorokhovtsev@gmail.com>
2010-03-09 17:27:40 +0300

    filter "unused photos" add moving to slide show fix

--- app/controllers/slideshows_controller.rb
+++ app/controllers/slideshows_controller.rb
@@ -14,10 +14,14 @@ class SlideshowsController < ApplicationController
     @slideshow = Slideshow.find(params[:id])
     session[:slideshow] = @slideshow
     @photos = unused_photos(@slideshow)
+    @all_categories = Category.find(:all, :order => "name")
   end
   
   def unused_photos(slideshow)
-    all_photos = Photo.find(:all)
+    category_id = session[:category_id] || 1
+    session[:category_id] = category_id
+    category = Category.find(category_id)
+    all_photos = category.photos_including_child_categories
     candidates = []
     for photo in all_photos
        in_slideshow = false
@@ -63,5 +67,29 @@ class SlideshowsController < ApplicationController
       @slide = @slideshow.slides[0]
     end
     render :partial => "show_slide"
-  end 
+  end
+  
+  def add_photo
+    slideshow_id = session[:slideshow].id
+    photo_id = params[:id].split("_")[1]
+    slide = Slide.new()
+    slide.photo_id = photo_id
+    slide.slideshow_id = slideshow_id
+    if !slide.save
+      flash[:notice] = 'Error: Unable to add photo.'
+    end
+    @slideshow = Slideshow.find(slideshow_id)
+    session[:slideshow] = @slideshow
+    render :partial => "show_slides_draggable"
+  end
+  
+  def change_filter
+    slideshow_id = session[:slideshow].id
+    category_id = params[:category_id] || 1
+    session[:category_id] = category_id
+    @slideshow = Slideshow.find(slideshow_id)
+    session[:slideshow] = @slideshow
+    @photos = unused_photos(@slideshow)
+    render :partial => "photo_picker"
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,24 @@
 class Category < ActiveRecord::Base
-    has_and_belongs_to_many :photos
-    acts_as_tree
+  has_and_belongs_to_many :photos
+  acts_as_tree
     
-    def ancestors_name
-      if parent
-        parent.ancestors_name + parent.name + ':'
-      else
-        ""
-      end
+  def ancestors_name
+    if parent
+      parent.ancestors_name + parent.name + ':'
+    else
+      ""
     end
-    
-    def long_name
-      ancestors_name + name
+  end
+  
+  def long_name
+    ancestors_name + name
+  end
+  
+  def photos_including_child_categories
+    result = photos.clone
+    children.each do |c|
+      c.photos_including_child_categories.each {|p| result << p if not result.include? p}
     end
+    result
+  end
 end
